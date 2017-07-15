4f0c4bdf5e16046bc40ae0346c1d99d732677a1c
chaitanyaaaa <chaitanyaaa@live.com>
2017-07-15 14:29:28 +0530

    addde new code

---
+++ basicjava.java
@@ -0,0 +1,37 @@
+Class HelloWorld
+{
+Void print()
+{
+	System.out.println("Hello World zxc")
+}
+
+
+
+ Public Static Void Main(String args[])
+{
+	HelloWorld hw = new HelloWorld();
+	hw.print();
+	NewClass nc = new NewClass();
+	hw.nc.newprint();
+}
+
+}
+
+
+Class NewClass
+{
+String str = "This is new class";
+void newprint()
+{
+System.out.println(str);
+}
+
+}
+
+
+
+-----------------------------------------------------
+
+
+Public class Javabasic{
+
